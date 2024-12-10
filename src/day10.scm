33965b4f3610a852e18e8aa5701581e5fef80f51
Emilio Ziniades <emilioziniades@protonmail.com>
2024-12-10 17:41:21 +0200

    day 10 tidy

--- src/day10.scm
+++ src/day10.scm
@@ -1,16 +1,24 @@
 (define-module (day10) #:export (trailhead-scores trailhead-ratings))
 
-(use-modules (util io) (util input) (srfi srfi-1))
+(use-modules (util io) (util input) (srfi srfi-1) (ice-9 curried-definitions))
 
 ;; part 1
 (define (trailhead-scores file)
  (let* ((arr (make-grid file))
         (trailheads (find-trailheads arr)))
-  (list-sum (map (lambda (th)
-                  (trailhead-score arr th))
-                 trailheads))))
+  (list-sum
+   ;; deduplicate reachable 9s for part 1
+   (map length (map list-distinct (map (reachable-9s arr) trailheads))))))
 
-(define (trailhead-score arr pos)
+;; part 2
+(define (trailhead-ratings file)
+ (let* ((arr (make-grid file))
+        (trailheads (find-trailheads arr)))
+  (list-sum (map length (map (reachable-9s arr) trailheads)))))
+
+;; common to both parts
+(define*
+ ((reachable-9s arr) pos)
  (define (helper pos)
   (let* ((x (first pos))
          (y (second pos))
@@ -31,40 +39,8 @@
     (list pos)
     (append-map helper valid-neighbours))))
 
- (length (list-distinct (helper pos))))
-
-;; part 2
-(define (trailhead-ratings file)
- (let* ((arr (make-grid file))
-        (trailheads (find-trailheads arr)))
-  (list-sum (map (lambda (th)
-                  (trailhead-rating arr th))
-                 trailheads))))
-
-(define (trailhead-rating arr pos)
- (define (helper pos)
-  (let* ((x (first pos))
-         (y (second pos))
-         (height (parse-int (apply array-ref (cons arr pos))))
-         (neighbours (list (list (1- x) y)
-                           (list (1+ x) y)
-                           (list x (1- y))
-                           (list x (1+ y))))
-         (valid-neighbours (filter
-                            (lambda (p)
-                             (and
-                              (apply array-in-bounds? (cons arr p))
-                              (equal?
-                               (1+ height)
-                               (parse-int (apply array-ref (cons arr p))))))
-                            neighbours)))
-   (if (equal? height 9)
-    1
-    (list-sum (map helper valid-neighbours)))))
-
  (helper pos))
 
-;; common to both parts
 (define (find-trailheads arr)
  (filter (lambda (pos)
           (equal? #\0 (apply array-ref (cons arr pos))))
